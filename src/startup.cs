1a096f369e429db178d96cfd776973130b53e67c
jose <1211089@isep.ipp.pt>
2023-12-06 14:18:50 +0000

    Add DbContext and API Explorer services

--- src/startup.cs
+++ src/startup.cs
@@ -2,28 +2,50 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using RobDroneGo.Infrastructure;
 
-namespace Application {
+namespace Application
+{
 
-    public class StartUp {
+    public class StartUp
+    {
 
-        public StartUp(IConfiguration configuration) {
+        public StartUp(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
 
-        public void ConfigureServices(IServiceCollection services) {
-            
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<RobDroneGoAuthContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+                 ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection"))));
+
+            services.AddEndpointsApiExplorer();
         }
 
-        public void Configure (IApplicationBuilder applicationBuilder, IWebHostEnvironment hostEnvironment) {
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment hostEnvironment)
+        {
+            if (hostEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                applicationBuilder.UseHsts();
+            }
 
-        }
-
-        public void ConfigureMyServices(IServiceCollection services) {
-
+            applicationBuilder.UseHttpsRedirection();
+            applicationBuilder.UseRouting();
+            //applicationBuilder.UseAuthorization();
+            //applicationBuilder.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
         }
 
     }
