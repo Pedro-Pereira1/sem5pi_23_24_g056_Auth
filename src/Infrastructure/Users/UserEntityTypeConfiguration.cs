ebb8832e495327db24a954d214bc8aa2df78a2ed
jose <1211089@isep.ipp.pt>
2023-12-06 22:12:31 +0000

    Add UserEntityTypeConfiguration for User entity

---
+++ src/Infrastructure/Users/UserEntityTypeConfiguration.cs
@@ -0,0 +1,42 @@
+using DDDSample1.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RobDroneGoAuth.Domain.Users;
+
+namespace RobDroneGoAuth.Infrastructure.Users
+{
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Id)
+                .HasConversion(
+                    v => v.Value,
+                    v => Email.Create(v));
+
+            builder.Property(b => b.Name)
+                .HasConversion(
+                    v => v.NameString,
+                    v => Name.Create(v));
+
+            builder.Property(b => b.Password)
+                .HasConversion(
+                    v => v.PasswordString,
+                    v => Password.Create(v));
+
+            builder.Property(b => b.PhoneNumber)
+                .HasConversion(
+                    v => v.Number,
+                    v => PhoneNumber.Create(v));
+
+
+            builder.Property(b => b.TaxPayerNumber)
+                .HasConversion(
+                    v => v.Number,
+                    v => TaxPayerNumber.Create(v));
+
+        }
+    }
+}
