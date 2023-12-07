81024fb609788cdb7e3453770dc624a91bb48b8b
jose <1211089@isep.ipp.pt>
2023-12-07 23:18:31 +0000

    SC-39 Add email validation rules

--- src/Domain/Users/Email.cs
+++ src/Domain/Users/Email.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using DDDSample1.Domain.Shared;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace RobDroneGoAuth.Domain.Users
 {
@@ -11,6 +12,18 @@ namespace RobDroneGoAuth.Domain.Users
 
         public static Email Create(string email)
         {
+            if (email == null)
+            {
+                throw new BusinessRuleValidationException("Email can't be null.");
+            }
+            if (!email.Contains('@'))
+            {
+                throw new BusinessRuleValidationException("Email must contain '@'.");
+            }
+            if (!email.Contains("isep.ipp.pt"))
+            {
+                throw new BusinessRuleValidationException("Email must be from 'isep.ipp.pt' domain.");
+            }
             return new Email(email);
         }
     }
