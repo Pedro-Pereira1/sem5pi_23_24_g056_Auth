fb952986c70648f25ca394dc826dd9e99bfda8fd
jose <1211089@isep.ipp.pt>
2023-12-07 18:23:34 +0000

    SC-39 Add UserController with RegisterUser endpoint

---
+++ src/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using RobDroneGoAuth.Domain.Users;
+using RobDroneGoAuth.Infrastructure.Users;
+
+namespace RobDroneGoAuth.Controllers.User
+{
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> RegisterUser([FromBody] CreateUserDto dto)
+        {
+            try
+            {
+                Console.WriteLine("Create User");
+                var user = await this._userService.RegisterUser(dto);
+                return user;
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
