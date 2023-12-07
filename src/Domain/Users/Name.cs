ab202e60dc64ffa5653bf6cd4d13445b7e6b882d
jose <1211089@isep.ipp.pt>
2023-12-07 23:38:57 +0000

    SC-39 Add validation for name in Create method

--- src/Domain/Users/Name.cs
+++ src/Domain/Users/Name.cs
@@ -15,6 +15,17 @@ namespace RobDroneGoAuth.Domain.Users
 
         public static Name Create(string name)
         {
+            Regex rx = new Regex(@"^[a-zA-Z ]+$");
+
+            if (name.Length < 1)
+            {
+                throw new BusinessRuleValidationException("Name must contain at least 1 character");
+            }
+            if (!rx.IsMatch(name))
+            {
+                throw new BusinessRuleValidationException("Name must contain only letters");
+            }
+
             return new Name(name);
         }
 
