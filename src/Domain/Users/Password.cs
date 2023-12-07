438a4ff43b8603b9b496ebd4428601868a74ae5b
jose <1211089@isep.ipp.pt>
2023-12-07 23:34:09 +0000

    SC-39 Add password validation rules

--- src/Domain/Users/Password.cs
+++ src/Domain/Users/Password.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using DDDSample1.Domain.Shared;
 
 namespace RobDroneGoAuth.Domain.Users
@@ -14,9 +15,30 @@ namespace RobDroneGoAuth.Domain.Users
 
         public static Password Create(String password)
         {
+            Regex uppercase = new Regex("[A-Z]");
+            Regex lowercase = new Regex("[a-z]");
+            Regex number = new Regex("[0-9]");
+            Regex special = new Regex("[!@#$%^&*()_+]");
+
             if (password.Length < 10)
             {
                 throw new BusinessRuleValidationException("Password must contain at least 10 characters");
+            }
+            if(uppercase.Matches(password).Count < 1)
+            {
+                throw new BusinessRuleValidationException("Password must contain at least one uppercase letter");
+            }
+            if (lowercase.Matches(password).Count < 1)
+            {
+                throw new BusinessRuleValidationException("Password must contain at least one lowercase letter");
+            }
+            if (number.Matches(password).Count < 1)
+            {
+                throw new BusinessRuleValidationException("Password must contain at least one number");
+            }
+            if (special.Matches(password).Count < 1)
+            {
+                throw new BusinessRuleValidationException("Password must contain at least one special character");
             }
             return new Password(password);
         }
