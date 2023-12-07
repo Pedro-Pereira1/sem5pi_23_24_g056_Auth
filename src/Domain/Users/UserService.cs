e2c5edab6082cb3a555466c65a5983e246e82b54
jose <1211089@isep.ipp.pt>
2023-12-07 22:40:35 +0000

    SC-39 Add logger to UserService constructor and handle exceptions in RegisterUser method

--- src/Domain/Users/UserService.cs
+++ src/Domain/Users/UserService.cs
@@ -7,9 +7,11 @@ namespace RobDroneGoAuth.Infrastructure.Users
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IUserRepository _userRepository;
+        private readonly ILogger<UserService> _logger;
 
-        public UserService(IUnitOfWork unitOfWork, IUserRepository userRepository)
+        public UserService(ILogger<UserService> logger, IUnitOfWork unitOfWork, IUserRepository userRepository)
         {
+            this._logger = logger;
             this._unitOfWork = unitOfWork;
             this._userRepository = userRepository;
         }
@@ -18,13 +20,21 @@ namespace RobDroneGoAuth.Infrastructure.Users
         {
             try
             {
+                _logger.LogInformation("UserService: Registering user\n\n");
+
                 var user = User.Create(dto.Name, dto.Email, dto.TaxPayerNumber, dto.PhoneNumber, dto.Password);
                 await this._userRepository.AddAsync(user);
                 await this._unitOfWork.CommitAsync();
                 return new UserDto(user.Name.NameString, user.Id.Value, user.PhoneNumber.Number, user.TaxPayerNumber.Number);
             }
+            catch (BusinessRuleValidationException e)
+            {
+                _logger.LogError("UserService: Error has occurred while registering user: " + e.Message);
+                throw new BusinessRuleValidationException(e.Message);
+            }
             catch (Exception e)
             {
+                _logger.LogError("UserService: Error has occurred while registering user: " + e.Message);
                 throw new Exception(e.Message);
             }
         }
