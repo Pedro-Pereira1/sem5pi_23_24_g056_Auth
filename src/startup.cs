5ac077a10f5c8ee17465bd86b36856e212262b98
JoseGouveia <1211089@isep.ipp.pt>
2023-12-07 11:28:37 +0000

    Add dependencies and services for user management

--- src/startup.cs
+++ src/startup.cs
@@ -2,7 +2,12 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using RobDroneGo.Infrastructure;
+using DDDSample1.Domain.Shared;
+using RobDroneGoAuth.Infrastructure;
+using RobDroneGoAuth.Domain.Users;
+using RobDroneGoAuth.Infrastructure.Users;
 
 namespace Application
 {
@@ -24,7 +29,13 @@ namespace Application
                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
                  ServerVersion.AutoDetect(Configuration.GetConnectionString("DefaultConnection"))));
 
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<UserService, UserService>();
+
             services.AddEndpointsApiExplorer();
+            services.AddControllers().AddNewtonsoftJson();
         }
 
         public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment hostEnvironment)
@@ -41,11 +52,11 @@ namespace Application
 
             applicationBuilder.UseHttpsRedirection();
             applicationBuilder.UseRouting();
-            //applicationBuilder.UseAuthorization();
-            //applicationBuilder.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllers();
-            //});
+            applicationBuilder.UseAuthorization();
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
 
     }
